0db728f5734eed9b32fbbedd3ff9e0954896775c
liming1 <2412780034@qq.com>
2023-08-29 17:00:03 +0800

    20230829

---
+++ 001_HelloWorld.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# print('hello', ',', 'world')
+# print(100+200)
+# print(2**10)
+# print('100 + 200 =', 100+200)
+
+# name = input()
+# print('you are so nice ,', name)
+
+# a = 100
+# if a >= 0:
+#     print(a)
+# else:
+#     print(-a)
+# print(10_000_000_000)
+# print(0x9d90_2c)
+# print('I\'m OK.')
+# print(-280.74)
+# print(1.23e9)
+# print('\\\n\\')
+# print(r'\\\t\\')
+# print('''line1
+# line2
+# line3''')
+# print(r'''hello\n
+# world''')
+
+# print(3 > 2)
+# print(3 < 2)
+# print(True and False)
+# print(True or False)
+# print(not True)
+# print(False or False)
+
+# age = int(input())
+# if age >= 18:
+#     print('adult')
+# else:
+#     print('teenager')
+
+
+# a = 123
+# b = 'char'
+# c = True
+# print(a, b, c)
+
+print(ord('A'))
+print(ord('中'))
+print(chr(66))
+print(chr(25991))
+
+
+print('\u4e2d\u6587')
