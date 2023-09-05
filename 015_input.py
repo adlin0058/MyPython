5fc06b3457b1d3d920fbe41f06db14721d078d02
liming1 <2412780034@qq.com>
2023-09-05 15:36:10 +0800

    20230905

--- 015_input.py
+++ 015_input.py
@@ -1,9 +1,35 @@
-# 键盘输入
+
+# todo 键盘输入
 # str = input('请输入：')
 # print('你输入的内容是：', str)
 
 
-# 读写文件
-f = open('015_foo.txt', 'w')
-f.write('Python是一个非常好的语言\n是的，的确非常好！\n')
-f.close()
+# todo 读写文件
+# f = open('015_foo.txt', 'w')
+# f.write('Python是一个非常好的语言\n是的，的确非常好！\n')
+# f.close()
+
+# todo f.read()
+# f = open('015_foo.txt', 'r')
+# str = f.read()
+# print(str)
+# f.close()
+
+# todo f.readline()
+# f = open('015_foo.txt', 'r')
+# str = f.readline()
+# print(str)
+# f.close()
+
+# todo f.readlines()
+# f = open('015_foo.txt', 'r')
+# str = f.readlines()
+# print(str)
+# f.close()
+
+# todo f.write()将string写入文件，返回字符数
+# f = open('015_foo.txt', 'w')
+# num = f.write('Python是一个非常好的语言\n')
+# print(num)
+
+# f.close()
