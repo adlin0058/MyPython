7307a1ba52b344f6e8dbfdada82465a54146f63d
liming1 <2412780034@qq.com>
2023-09-14 13:42:39 +0800

    20230914

--- Example/020_Convert.py
+++ Example/020_Convert.py
@@ -6,3 +6,10 @@ dec = int(input('请输入一个十进制数:'))
 print('转换为二进制为：', bin(dec))
 print('转换成八进制为：', oct(dec))
 print('转换成十六进制为：', hex(dec))
+
+# todo ASCII、字符项目转换
+
+c = input('请输入一个字符：')
+a = int(input('请输入一个ASCII码：'))
+print(c, '的ASCII码是：', ord(c))
+print(a, '对应的字符为：', chr(a))
