78a4aca429f5787005aa3af7f5aba38777b55bac
liming1 <2412780034@qq.com>
2023-09-15 09:37:30 +0800

    20230915

---
+++ Example/026_Str.py
@@ -0,0 +1,16 @@
+# 字符串判断
+
+# str = "runoob.com"
+# print(str.isalnum())
+# print(str.isalpha())
+# print(str.isdigit())
+# print(str.islower())
+# print(str.isupper())
+# print(str.istitle())
+# print(str.isspace())
+
+str = 'www.runoob.com'
+print(str.upper())
+print(str.lower())
+print(str.capitalize())
+print(str.title())
