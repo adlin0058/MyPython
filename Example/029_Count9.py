b764a5fd68af3415d3eb7065fe4c0b551397bd56
liming1 <2412780034@qq.com>
2023-09-15 14:29:50 +0800

    20230915

--- Example/029_Count9.py
+++ Example/029_Count9.py
@@ -2,18 +2,31 @@
 # 报数，从 1 开始，数到 9 的人下船
 # 直到船上仅剩 15 人为止
 # 问都有哪些编号的人下船了呢
-
 people = {}
 for x in range(1, 31):
     people[x] = 1
-
 # print(people)
+
+# 用于循环遍历字典 1——31 值为31再次进入循环时会变为1
+i = 1
+# 用于报数 1-9 值为9时变为1
 cnt = 0
-x = 0
-while cnt < 15:
-    if people[x] == 1:
+# 用于计数下船几个
+n = 0
+while i <= 31:
+    if i == 31:
+        i = 1
+
+    if people[i] == 0:
+        i += 1
+        continue
+    elif people[i] == 1:
+        cnt += 1
         if cnt == 9:
-            people[x] = 0
-            print(x, '号下船了')
-            cnt += 1
-        x += 1
+            people[i] = 0
+            print(i, '下船')
+            cnt = 0
+            n += 1
+        i += 1
+    if n == 15:
+        break
