d68530bafca620184912455acb1fb18d3d88ece9
liming1 <2412780034@qq.com>
2023-09-18 10:52:15 +0800

    20230918

--- Example/041_OccurTimes.py
+++ Example/041_OccurTimes.py
@@ -12,3 +12,8 @@ l = [1, 2, 1, 1, 5, 6, 72, 8, 2]
 print(Times(l, 1))
 print(Times(l, 1))
 print(Times(l, 99))
+
+
+# 还可以直接使用count()方法
+cnt = l.count(1)
+print(cnt)
---
+++ Example/042_RemoveStr.py
@@ -0,0 +1,9 @@
+# 移除字符串中的指定位置元素
+
+s = 'lmshigedashuaibi'
+
+l = list(s)
+l.pop(0)
+# 将数组变为str
+s = ''.join(l)
+print(s)
