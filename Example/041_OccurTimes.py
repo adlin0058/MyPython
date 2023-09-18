164f1fe5f9b188e791baf7cc4b8686ccab7002f9
liming1 <2412780034@qq.com>
2023-09-18 10:34:03 +0800

    20230918

---
+++ Example/041_OccurTimes.py
@@ -0,0 +1,14 @@
+# 计算元素在列表中出现的次数
+
+def Times(l, e):
+    cnt = 0
+    for x in l:
+        if x == e:
+            cnt += 1
+    return cnt
+
+
+l = [1, 2, 1, 1, 5, 6, 72, 8, 2]
+print(Times(l, 1))
+print(Times(l, 1))
+print(Times(l, 99))
