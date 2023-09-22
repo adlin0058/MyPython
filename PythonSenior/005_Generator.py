2cb0a35818c2ba402ede5bfe3a7ec887201727bb
liming1 <2412780034@qq.com>
2023-09-22 13:13:12 +0800

    20230922

--- PythonSenior/005_Generator.py
+++ PythonSenior/005_Generator.py
@@ -1,6 +1,41 @@
 # 迭代器
-g = (x*x for x in range(10))
-print(g)
+# g = (x*x for x in range(10))
+# print(g)
 
-for x in g:
+# for x in g:
+#     print(x)
+
+
+# def fib(max):
+#     n, a, b = 0, 0, 1
+#     while n < max:
+#         print(b)
+#         a, b = b, a+b
+#         n = n+1
+#     return 'done'
+
+
+# fib(6)
+
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a+b
+        n = n+1
+    return 'done'
+
+
+for x in fib(6):
     print(x)
+
+
+g = fib(6)
+while True:
+    try:
+        x = next(g)
+        print('g:', x)
+    except StopIteration as e:
+        print('Generator return value:', e.value)
+        break
