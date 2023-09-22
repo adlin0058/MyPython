cb5182c2c98f65ad884e3556ed57f2cab5772c9e
liming1 <2412780034@qq.com>
2023-09-22 15:51:38 +0800

    20230922

--- PythonSenior/006_YanghuiTriangle.py
+++ PythonSenior/006_YanghuiTriangle.py
@@ -1,6 +1,22 @@
 # 杨辉三角
 
-def triangle():
-    yield (result := [1])
-    while True:
-        yield (result := [a+b for a, b in zip(result + [0], [0]+result)])
+def triangles(n):
+    yang = []
+    i = 0  # 第i层
+    while i < n:
+        j = 0  # 第i层的第j个元素
+        hang = []  # 第j行的元素
+        while j <= i:
+            if j == 0 or j == i:
+                hang.append(1)
+            else:
+                hang.append(yang[i-1][j-1]+yang[i-1][j])
+            j += 1
+        yang.append(hang)
+        i += 1
+    return yang
+
+
+y = triangles(5)
+for x in y:
+    print(x)
