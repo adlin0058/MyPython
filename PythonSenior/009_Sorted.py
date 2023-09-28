2fd3cf78cdd7985e94a9235f9de48a3430674ff7
liming1 <2412780034@qq.com>
2023-09-28 10:11:33 +0800

    20230928

--- PythonSenior/009_Sorted.py
+++ PythonSenior/009_Sorted.py
@@ -10,6 +10,9 @@
 # # 使用key忽略对字符串排序时的大小写
 # l1 = ['bob', 'about', 'Zoo', 'Credit']
 # print(sorted(l1, key=str.lower))
+# # 反向排序
+# l3 = sorted(l1, key=str.lower, reverse=True)
+# print(l3)
 
 L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
 
@@ -20,3 +23,11 @@ def by_name(t):
 
 L2 = sorted(L, key=by_name)
 print(L2)
+
+
+def by_sorce(t):
+    return t[1]
+
+
+L3 = sorted(L, key=by_sorce, reverse=True)
+print(L3)
