07fdbf8f4edf2e7a51cf1d4e09f89ab04a7646cb
liming1 <2412780034@qq.com>
2023-10-07 16:15:27 +0800

    20231007

--- PythonSenior/010_ReturnFunc.py
+++ PythonSenior/010_ReturnFunc.py
@@ -21,3 +21,40 @@ f = lazy_sum(1, 3, 5, 7, 9)
 print(f)
 # 调用f时才计算求和结果
 print(f())
+
+# 每次调用时返回一个新的函数
+f1 = lazy_sum(1, 3, 5, 7, 9)
+f2 = lazy_sum(1, 3, 5, 7, 9)
+print(f1 == f2)
+
+
+def count():
+    fs = []
+    for i in range(1, 4):
+        def f():
+            return i*i
+        fs.append(f)
+    return fs
+
+
+f1, f2, f3 = count()
+print(f1())
+print(f2())
+print(f3())
+
+
+def count():
+    def f(j):
+        def g():
+            return j*j
+        return g
+    fs = []
+    for i in range(1, 4):
+        fs.append(f(i))  # f(i)立刻被执行，因此i的当前值被传入f()
+    return fs
+
+
+f1, f2, f3 = count()
+print(f1())
+print(f2())
+print(f3())
