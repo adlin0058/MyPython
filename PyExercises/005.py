7dc39cc944c5c3246ef4e8fed68cf8de797e0ba9
liming1 <2412780034@qq.com>
2023-10-27 11:00:49 +0800

    20231027

--- PyExercises/005.py
+++ PyExercises/005.py
@@ -6,9 +6,26 @@
 # print(L[:2])
 
 
-def twoSum(nums: list(int), target: int):
+# def twoSum(nums: list(int), target: int):
+#     dic = {}
+#     for i, x in enumerate(nums):
+#         for j in range(i+1, len(nums)):
+#             if x + nums[j] == target:
+#                 return [i, j]
+
+
+def twoSum(nums, target):
     dic = {}
+
     for i, x in enumerate(nums):
-        for j in range(i+1, len(nums)):
-            if x + nums[j] == target:
-                return [i, j]
+        complement = target - x
+        if complement in dic:
+            return [dic[complement], i]
+        dic[x] = i
+
+    return []
+
+
+nums = [2, 7, 11, 15]
+l = twoSum(nums, 9)
+print(l)
