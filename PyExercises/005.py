7abe4256eacd100a5c6f5f5eca6441a671f4587a
liming1 <2412780034@qq.com>
2023-10-27 10:33:32 +0800

    20231027

--- PyExercises/005.py
+++ PyExercises/005.py
@@ -1,6 +1,14 @@
 
-L = [2, 5, 7, 4]
-L.sort(reverse=False)
+# L = [2, 5, 7, 4]
+# L.sort(reverse=False)
 
-print(L)
-print(L[:2])
+# print(L)
+# print(L[:2])
+
+
+def twoSum(nums: list(int), target: int):
+    dic = {}
+    for i, x in enumerate(nums):
+        for j in range(i+1, len(nums)):
+            if x + nums[j] == target:
+                return [i, j]
