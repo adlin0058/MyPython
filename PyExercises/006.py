4868d94345e8220506ac580673fbe6a9cc3de23f
liming1 <2412780034@qq.com>
2023-10-27 13:46:26 +0800

    20231027

--- PyExercises/006.py
+++ PyExercises/006.py
@@ -5,3 +5,9 @@ def canConstruct(self, ransomNote: str, magazine: str) -> bool:
     if len(ransomNote) > len(magazine):
         return False
     return not collections.Counter(ransomNote) - collections.Counter(magazine)
+
+
+ransomNote = "aa"
+magazine = "aab"
+print(collections.Counter(ransomNote))
+print(collections.Counter(magazine))
