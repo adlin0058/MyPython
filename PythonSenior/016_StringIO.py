64dbdd058d4434da51b2d92620d1165b127d5ebd
liming1 <2412780034@qq.com>
2023-11-01 16:06:46 +0800

    20231101

---
+++ PythonSenior/016_StringIO.py
@@ -0,0 +1,21 @@
+# from io import StringIO
+
+
+# # f = StringIO()
+# # f.write('hello')
+# # print(f.getvalue())
+
+# f = StringIO('Hello!\nHi!\nGoodbye!')
+# while True:
+#     s = f.readline()
+#     if s == '':
+#         break
+#     print(s.strip())
+
+
+from io import BytesIO
+# f = BytesIO()
+# f.write('中文'.encode('utf-8'))
+# print(f.getvalue())
+f = BytesIO(b'\xe4\xb8\xad\xe6\x96\x87')
+f.read()
