bc5134d19b3a05f622bad67874698fb6c91a1b55
liming1 <2412780034@qq.com>
2023-11-06 17:00:26 +0800

    20231106

--- PythonSenior/019_Turtle.py
+++ PythonSenior/019_Turtle.py
@@ -2,24 +2,43 @@
 #图形化界面
 from turtle import *
 
-#设置笔刷宽度
-width(4)
+# #设置笔刷宽度
+# width(4)
 
-#前进
-forward(200)
-#右转90度：
-right(90)
+# #前进
+# forward(200)
+# #右转90度：
+# right(90)
 
-#笔刷颜色
-pencolor('red')
+# #笔刷颜色
+# pencolor('red')
 
-forward(100)
-right(90)
+# forward(100)
+# right(90)
 
-pencolor('green')
-forward(200)
-right(90)
+# pencolor('green')
+# forward(200)
+# right(90)
 
-pencolor('blue')
-forward(100)
-right(90)
+# pencolor('blue')
+# forward(100)
+# right(90)
+
+# done()
+
+
+def drawStar(x,y):
+    pu()
+    goto(x,y)
+    pd()
+    
+    #set heading:0
+    seth(0)
+    for i in range(6):
+        fd(60)
+        rt(60)
+        
+for x in range(-450,450,150):
+    drawStar(x,0)
+    
+done()
