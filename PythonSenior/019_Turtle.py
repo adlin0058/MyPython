82ed6ae76f258a28602b0e7e26230adfe0798513
liming1 <2412780034@qq.com>
2023-11-13 14:57:23 +0800

    20231113

--- PythonSenior/019_Turtle.py
+++ PythonSenior/019_Turtle.py
@@ -27,18 +27,19 @@ from turtle import *
 # done()
 
 
-def drawStar(x,y):
+def drawStar(x,y,n):
     pu()
     goto(x,y)
     pd()
     
     #set heading:0
     seth(0)
-    for i in range(6):
+    for i in range(n):
         fd(60)
-        rt(60)
-        
-for x in range(-450,450,150):
-    drawStar(x,0)
+        rt(360/n)
+
+n = int(input('请输入边数'))   
+for x in range(-400,400,200):
+    drawStar(x,0,n)
     
-done()
+done() 
